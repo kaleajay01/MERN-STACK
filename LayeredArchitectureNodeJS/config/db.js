b4c5afe8c855a9695e3e4205860641e64ce4a030
ajay kale <ajaykale749810@gmail.com>
2026-02-09 23:25:53 +0530

    modify db connection

--- LayeredArchitectureNodeJS/config/db.js
+++ LayeredArchitectureNodeJS/config/db.js
@@ -5,15 +5,3 @@ const dbserver={
     password: "password",
     database: 'customers'
 };
-const connection=mysql.createConnection(dbserver);
-connection.connect(function(err){
-    if(err){
-        console.log("Connection Unsucessful.."+err)
-    }
-    else{
-        console.log("Connection Sucessful.." )
-    }
-});
-module.exports = connection;
-
-//db connection
