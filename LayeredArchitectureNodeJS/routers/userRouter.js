0432a10c8cdb35e20736606015f6a80453ac5e3e
ajay kale <ajaykale749810@gmail.com>
2026-02-26 21:58:14 +0530

    Nothing

--- LayeredArchitectureNodeJS/routers/userRouter.js
+++ LayeredArchitectureNodeJS/routers/userRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
+
+
+
 router.post('/users', controller.addUser);
 router.delete('/delete/:id', controller.deleteUser);
 router.get('/getAllUsers', controller.getUsers);
