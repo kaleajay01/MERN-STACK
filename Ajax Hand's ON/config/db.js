a2d3f7dd57037eb91c47d62fe812e2191bc15a32
Ajay Kale <ajaykale749810@gmail.com>
2026-03-17 21:26:14 +0530

    Update db.js

--- Ajax Hand's ON/config/db.js
+++ Ajax Hand's ON/config/db.js
@@ -7,13 +7,4 @@ const dbserver = {
   database: "ajax"
 };
 
-const connection = mysql.createConnection(dbserver);
-connection.connect((err) => {
-  if (err) {
-    console.error("connection failed:", err);
-  } else {
-    console.log("MySQL connected");
-  }
-});
-
 module.exports = connection;
